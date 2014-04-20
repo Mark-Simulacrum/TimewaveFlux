18d4b298d51e0ee2b3fea00c33fa26a595996c7f
Mark Simulacrum <mark.simulacrum@gmail.com>
2014-04-20 10:14:39 -0600

    Changed Much.

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,29 @@
 var gulp = require('gulp');
-var connect = require('gulp-connect');
+// var connect = require('gulp-connect');
+var sass = require('gulp-ruby-sass');
 
 
-gulp.task('connect', function() {
-	connect.server({
-		root: ['.'],
-		livereload: true
-	});
-});
+// gulp.task('connect', function() {
+// 	connect.server({
+// 		root: ['.'],
+// 		livereload: true
+// 	});
+// });
 
-gulp.task('connect-reload', function () {
-  gulp.src('./**')
-    .pipe(connect.reload());
-});
+// gulp.task('connect-reload', function () {
+//   gulp.src('./*.html')
+//     .pipe(connect.reload());
+// });
 
-gulp.task('connect-reload', function() {
-	connect.reload();
+gulp.task('sass', function() {
+	return gulp.src('stylesheet.scss')
+		.pipe(sass({noCache: true}))
+		.pipe(gulp.dest('.'));
 });
 
 gulp.task('watch', function () {
-	gulp.watch(['index.html', 'javascript.js'], ['connect-reload'])
+	// gulp.watch(['index.html', 'javascript.js', 'stylesheet.css'], ['connect-reload']);
+	gulp.watch(['stylesheet.scss'], ['sass']);
 });
 
-gulp.task('default', ['connect', 'watch']);
-// gulp.task('default', ['connect']);
+gulp.task('default', ['watch']);
--- javascript.js
+++ javascript.js
@@ -1,39 +1,111 @@
-var divs = [];
-var mouse = {x: 0, y: 0};
-var moveDiv = false;
+"use strict";
 
-function updateDivs() {
-	divs.forEach(function(element) {
-		if (element["move"]) {
-			element["div"].style.top = (mouse.y - element["div"].clientHeight/2) + 'px'; // Subtract height/2 to move by center
-			element["div"].style.left = (mouse.x - element["div"].clientWidth/2) + 'px';
-		}
-	});
+var canvas = document.getElementById('background');
+var mouse = {x: 0, y: 0};
+var dayCount = 10;
+var screenWidth = document.body.clientWidth;
+var fullDayWidth = Math.floor(screenWidth/dayCount);
+var borderWidth = 3;
+var projectPadding = 0;
+var projectHeight = 100;
+var headerSize = 40;
+var hourHeight = 100;
+
+// Better implementation of [].forEach.call(...)
+var forEach = function (array, callback, scope) {
+  for (var i = 0; i < array.length; i++) {
+    callback.call(scope, i, array[i]); // passes back stuff we need
+  }
+};
+
+var projects = [
+	{
+		name: 'Project #1',
+		deadline: 0,
+		size: 2,
+		color: 'lightblue'
+	},
+	{
+		name: 'Project #2',
+		deadline: 2,
+		size: 4,
+		color: '#BADA55'
+	},
+	{
+		name: 'Project #3',
+		deadline: 1,
+		size: 4,
+		color: 'cyan'
+	}
+];
+
+// Create days
+canvas.width = screenWidth;
+canvas.height = window.innerHeight;
+var ctx = canvas.getContext('2d')
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, screenWidth, window.innerHeight);
+for (var dayNo = 0; dayNo < dayCount; dayNo++) {
+	drawBorder(ctx, dayNo);
+
+	ctx.textAlign = 'center';
+	ctx.font = 'bold 12pt Arial';
+	ctx.fillStyle = 'black';
+	ctx.fillText(dateText(dayNo), dayStart(dayNo) + dayWidth()/2, 20);
+};
+
+drawBorder(ctx, dayCount);
+
+
+forEach(projects, function createProjectDiv(index, project) {
+	var projectDiv = document.createElement('div');
+	
+	projectDiv.classList.add('project');
+
+	// project.deadline = Math.floor(Math.random()*dayCount);
+	// project.size = Math.ceil(Math.random()*dayCount/2);
+
+	var projectStart = project.deadline;
+
+
+
+	var projectWidth = project.size*(fullDayWidth) - borderWidth;
+	var projectEnd = dayStart(project.start);
+	var projectHeight = project.size*hourHeight;
+	
+	projectDiv.style.backgroundColor = project.color;
+	projectDiv.style.opacity = 0.9;
+	projectDiv.style.width = projectWidth + 'px';
+	projectDiv.style.top = (projectHeight + projectPadding)*index + headerSize + 'px';
+	projectDiv.style.left = dayStart(projectStart) + 'px';
+	
+	projectDiv.innerHTML = '<p>' + project.name + '<br>' + 'Deadline: ' + project.deadline + '<br>Size: ' + project.size + '</p>';
+
+	projectDiv = document.body.appendChild(projectDiv);
+});
+
+function leftBorder(dayNo) {
+	return dayNo*fullDayWidth;
 }
 
-// XXX: Find a way to change this mess of Array.proto.slice... to something nicer.
-Array.prototype.slice.call(document.querySelectorAll("div")).forEach(
-	function addDiv(value) {
-		var div = {
-			div: value,
-			move: false
-		}
-		
-		div["div"].addEventListener('mousedown', function(e) {
-			div["move"] = true;
-		}, false);
+function dayStart(dayNo) {
+	return leftBorder(dayNo) + borderWidth;
+}
 
-		div["div"].addEventListener('mouseup', function(e) {
-			div["move"] = false;
-		}, false);
+function dateText(dayNo) {
+	return dayNo;
+}
 
-		divs.push(div);
-	}
-);
+function dayWidth() {
+	return fullDayWidth - borderWidth;
+}
+
+function drawBorder(ctx, dayNo) {
+	ctx.fillStyle = 'red';
+	ctx.fillRect(leftBorder(dayNo), 0, borderWidth, window.innerHeight);
+}
 
 document.addEventListener('mousemove', function(e){ 
 	mouse.x = e.clientX || e.pageX; 
 	mouse.y = e.clientY || e.pageY;
-
-	updateDivs();
 }, false);
