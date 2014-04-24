4095798e29ea016a4a98c0b1729becbbbb168b09
Mark Simulacrum <mark.simulacrum@gmail.com>
2014-04-23 19:29:03 -0600

    added logic to add divs properly

--- javascript.js
+++ javascript.js
@@ -1,15 +1,16 @@
 "use strict";
 
-var canvas = document.getElementById('background');
+var canvas, screenWidth, fullDayWidth, timeout;
+
 var mouse = {x: 0, y: 0};
 var dayCount = 10;
-var screenWidth = document.body.clientWidth;
-var fullDayWidth = Math.floor(screenWidth/dayCount);
-var borderWidth = 3;
+var borderWidth = 1;
 var projectPadding = 0;
 var projectHeight = 100;
 var headerSize = 40;
 var hourHeight = 100;
+var scheduledProjects = 0;
+var daySize = 2;
 
 // Better implementation of [].forEach.call(...)
 var forEach = function (array, callback, scope) {
@@ -22,67 +23,22 @@ var projects = [
 	{
 		name: 'Project #1',
 		deadline: 0,
-		size: 2,
+		size: 1,
 		color: 'lightblue'
 	},
 	{
 		name: 'Project #2',
-		deadline: 2,
-		size: 4,
+		deadline: 0,
+		size: 1,
 		color: '#BADA55'
 	},
 	{
 		name: 'Project #3',
 		deadline: 1,
-		size: 4,
+		size: 1,
 		color: 'cyan'
-	}
+	},
 ];
-
-// Create days
-canvas.width = screenWidth;
-canvas.height = window.innerHeight;
-var ctx = canvas.getContext('2d')
-ctx.fillStyle = 'white';
-ctx.fillRect(0, 0, screenWidth, window.innerHeight);
-for (var dayNo = 0; dayNo < dayCount; dayNo++) {
-	drawBorder(ctx, dayNo);
-
-	ctx.textAlign = 'center';
-	ctx.font = 'bold 12pt Arial';
-	ctx.fillStyle = 'black';
-	ctx.fillText(dateText(dayNo), dayStart(dayNo) + dayWidth()/2, 20);
-};
-
-drawBorder(ctx, dayCount);
-
-
-forEach(projects, function createProjectDiv(index, project) {
-	var projectDiv = document.createElement('div');
-	
-	projectDiv.classList.add('project');
-
-	// project.deadline = Math.floor(Math.random()*dayCount);
-	// project.size = Math.ceil(Math.random()*dayCount/2);
-
-	var projectStart = project.deadline;
-
-
-
-	var projectWidth = project.size*(fullDayWidth) - borderWidth;
-	var projectEnd = dayStart(project.start);
-	var projectHeight = project.size*hourHeight;
-	
-	projectDiv.style.backgroundColor = project.color;
-	projectDiv.style.opacity = 0.9;
-	projectDiv.style.width = projectWidth + 'px';
-	projectDiv.style.top = (projectHeight + projectPadding)*index + headerSize + 'px';
-	projectDiv.style.left = dayStart(projectStart) + 'px';
-	
-	projectDiv.innerHTML = '<p>' + project.name + '<br>' + 'Deadline: ' + project.deadline + '<br>Size: ' + project.size + '</p>';
-
-	projectDiv = document.body.appendChild(projectDiv);
-});
 
 function leftBorder(dayNo) {
 	return dayNo*fullDayWidth;
@@ -92,12 +48,13 @@ function dayStart(dayNo) {
 	return leftBorder(dayNo) + borderWidth;
 }
 
-function dateText(dayNo) {
-	return dayNo;
-}
 
 function dayWidth() {
 	return fullDayWidth - borderWidth;
+}
+
+function dateText(dayNo) {
+	return dayNo;
 }
 
 function drawBorder(ctx, dayNo) {
@@ -105,7 +62,144 @@ function drawBorder(ctx, dayNo) {
 	ctx.fillRect(leftBorder(dayNo), 0, borderWidth, window.innerHeight);
 }
 
+function dayLoad(dayNo) {
+	var dayLoad = 0;
+
+	for (var i = 0; i < scheduledProjects; i++) {
+		var project = projects[i];
+		if (project.start <= dayNo && dayNo <= project.end) {
+			dayLoad += project.days[dayNo - project.start];
+		}
+	}
+
+	return dayLoad;
+}
+
+function newProject(name, deadline, size, color) {
+	color = color || 'red';
+	var project = {
+		'name': name,
+		'deadline': deadline,
+		'size': size,
+		'color': color
+	};
+
+	// var newProject = document.createElement('div');
+	// newProject.classList.add('project', 'not-scheduled');
+
+	// newProject.style.backgroundColor = project.color;
+	// newProject.style.opacity = 0.6;
+
+	// newProject.style.top = '200px';
+	// newProject.style.left = '200px';
+
+	// newProject.innerHTML = '<p>' + project.name + '<br>' + 'Deadline: ' + project.deadline + '<br>Size: ' + project.size + '</p>';
+	var newProject = createProjectDiv(project);
+
+	newProject.classList.add('not-scheduled');
+
+	newProject = document.getElementById('projects').appendChild(newProject);
+	newProject.style.top = newProject.offsetTop + hourHeight*daySize;
+
+	newProject.addEventListener('mousedown', function(){
+		newProject.offsetTop = mouse.y;
+		newProject.offsetLeft = mouse.x;
+
+		// projects.push(project);
+		// draw();
+	}, false);
+
+	return newProject;
+}
+
+function createProjectDiv(project) {
+	var projectWidth = fullDayWidth;
+	var projectHeight = project.size*hourHeight;
+
+
+	var projectDiv = document.createElement('div');
+	
+	projectDiv.classList.add('project');
+	projectDiv.id += 'project-' + project.id;
+
+	projectDiv.style.backgroundColor = project.color;
+	projectDiv.style.opacity = 0.6;
+	projectDiv.style.width = projectWidth + 'px';
+	projectDiv.style.height = projectHeight + 'px';
+
+	
+	projectDiv.innerHTML = '<p>' + project.name + '<br>' + 'Deadline: ' + project.deadline + '<br>Size: ' + project.size + '</p>';
+
+	return projectDiv;
+}
+
+var calcProjectVars = function calcProjectVars(index, project) {
+	project.start = project.deadline;
+	project.end = project.deadline;
+	project.id = index;
+	
+	while (dayLoad(project.start) + project.size > daySize && project.start > 0) {
+		--project.start;
+		--project.end;
+	}
+
+	project.days = [];
+
+	project.days.push(project.size);
+
+	var projectDiv = createProjectDiv(project);
+
+	
+	var projectTop = dayLoad(project.start)*hourHeight + headerSize;
+	var projectLeft = dayStart(project.start);
+	projectDiv.style.top = projectTop + 'px';
+	projectDiv.style.left = projectLeft + 'px';
+
+	document.getElementById('projects').appendChild(projectDiv);
+
+	++scheduledProjects;
+}
+
+function createDays() {
+	canvas.width = screenWidth;
+	canvas.height = window.innerHeight;
+	var ctx = canvas.getContext('2d')
+	// ctx.fillStyle = 'white';
+	// ctx.fillRect(0, 0, screenWidth, window.innerHeight);
+	ctx.fillStyle = 'red';
+	ctx.fillRect(0, headerSize, leftBorder(dayCount), borderWidth);
+	ctx.fillRect(0, daySize*hourHeight + headerSize, leftBorder(dayCount), borderWidth);
+	drawBorder(ctx, dayCount);
+	for (var dayNo = 0; dayNo < dayCount; dayNo++) {
+		drawBorder(ctx, dayNo);
+
+		ctx.textAlign = 'center';
+		ctx.font = 'bold 12pt Arial';
+		ctx.fillStyle = 'black';
+		ctx.fillText(dateText(dayNo), dayStart(dayNo) + dayWidth()/2, 20);
+	};
+}
+
+function draw() {
+	// (Re)define global variables.
+	canvas = document.getElementById('background');
+	screenWidth = document.body.clientWidth;
+	fullDayWidth = Math.floor(screenWidth/dayCount);
+	
+	createDays(); // XXX: Should clear before draw; perhaps check if need to
+
+	scheduledProjects = 0; // Reset scheduled projects.
+	document.getElementById('projects').innerHTML = ''; // Empty created projects.
+	forEach(projects, calcProjectVars);
+}
+
+
 document.addEventListener('mousemove', function(e){ 
 	mouse.x = e.clientX || e.pageX; 
 	mouse.y = e.clientY || e.pageY;
 }, false);
+
+// document.addEventListener('DOMContentLoaded', addthing, true); // Draw projects, etc.
+window.addEventListener('resize', draw, false);
+window.addEventListener('scroll', draw, false);
+document.addEventListener('DOMContentLoaded', draw, true); // Draw projects, etc.
