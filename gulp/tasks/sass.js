893b4fd50fa044d191370867c0ab161610790023
Mark-Simulacrum <mark.simulacrum@gmail.com>
2014-09-01 09:16:54 -0600

    Add autoprefixer to sass compiling.

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
 var handleErrors = require('../util/handleErrors');
 
 gulp.task('sass', function () {
-  return gulp.src('src/css/**/*.scss')
-    .pipe(sass({
-		sourceComments: 'normal'
-	}))
-    .on('error', handleErrors)
-    .pipe(gulp.dest('build'));
+	return gulp.src('src/css/**/*.scss')
+		.pipe(sass({
+			sourceComments: 'normal'
+		}))
+		.on('error', handleErrors)
+		.pipe(prefix('> 1%'))
+		.pipe(gulp.dest('build'));
 });
